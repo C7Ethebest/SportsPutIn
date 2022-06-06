096a4356fa171b367e0ed7331ee269d43f9343e3
LiEnglish <2961505633@qq.com>
2022-06-07 00:12:58 +0800

    MyAccount

--- src/views/MyAccount.vue
+++ src/views/MyAccount.vue
@@ -2,6 +2,7 @@
 
 
   <el-descriptions title="用户信息">
+
     <el-descriptions-item label="用户名">AAAA</el-descriptions-item>
     <el-descriptions-item label="手机号">18100000000</el-descriptions-item>
     <el-descriptions-item label="居住地">苏州市</el-descriptions-item>
@@ -9,6 +10,249 @@
       <el-tag size="small">学校</el-tag>
     </el-descriptions-item>
     <el-descriptions-item label="联系地址">江苏省苏州市吴中区吴中大道 1188 号</el-descriptions-item>
+
+    <el-descriptions-item label="用户名">ABBB</el-descriptions-item>
+    <el-descriptions-item label="手机号">18178887309</el-descriptions-item>
+    <el-descriptions-item label="居住地">北京市</el-descriptions-item>
+    <el-descriptions-item label="备注">
+      <el-tag size="small">学校</el-tag>
+    </el-descriptions-item>
+    <el-descriptions-item label="联系地址">北京市朝阳区安家楼村 809号</el-descriptions-item>
+
+    <el-descriptions-item label="用户名">AABB</el-descriptions-item>
+    <el-descriptions-item label="手机号">18432444762</el-descriptions-item>
+    <el-descriptions-item label="居住地">天津市</el-descriptions-item>
+    <el-descriptions-item label="备注">
+      <el-tag size="small">学校</el-tag>
+    </el-descriptions-item>
+    <el-descriptions-item label="联系地址">天津市津南区大韩庄</el-descriptions-item>
+
+    <el-descriptions-item label="用户名">ABCD</el-descriptions-item>
+    <el-descriptions-item label="手机号">12465890121</el-descriptions-item>
+    <el-descriptions-item label="居住地">长春市</el-descriptions-item>
+    <el-descriptions-item label="备注">
+      <el-tag size="small">学校</el-tag>
+    </el-descriptions-item>
+    <el-descriptions-item label="联系地址">吉林省长春市黎明村 1街道104号</el-descriptions-item>
+
+    <el-descriptions-item label="用户名">ABNM</el-descriptions-item>
+    <el-descriptions-item label="手机号">12569888089</el-descriptions-item>
+    <el-descriptions-item label="居住地">西安市</el-descriptions-item>
+    <el-descriptions-item label="备注">
+      <el-tag size="small">学校</el-tag>
+    </el-descriptions-item>
+    <el-descriptions-item label="联系地址">陕西省西安市未央区西马旗寨 2路209号</el-descriptions-item>
+
+    <el-descriptions-item label="用户名">HSUN</el-descriptions-item>
+    <el-descriptions-item label="手机号">15390908999</el-descriptions-item>
+    <el-descriptions-item label="居住地">南昌市</el-descriptions-item>
+    <el-descriptions-item label="备注">
+      <el-tag size="small">学校</el-tag>
+    </el-descriptions-item>
+    <el-descriptions-item label="联系地址">江西省南昌市西湖区朝阳小区 5单元504号</el-descriptions-item>
+
+    <el-descriptions-item label="用户名">ZKII</el-descriptions-item>
+    <el-descriptions-item label="手机号">15315627888</el-descriptions-item>
+    <el-descriptions-item label="居住地">北京市</el-descriptions-item>
+    <el-descriptions-item label="备注">
+      <el-tag size="small">学校</el-tag>
+    </el-descriptions-item>
+    <el-descriptions-item label="联系地址">北京市朝阳区霞光里小区 1单元106</el-descriptions-item>
+
+    <el-descriptions-item label="用户名">NHIA</el-descriptions-item>
+    <el-descriptions-item label="手机号">15377188820</el-descriptions-item>
+    <el-descriptions-item label="居住地">兰州市</el-descriptions-item>
+    <el-descriptions-item label="备注">
+      <el-tag size="small">学校</el-tag>
+    </el-descriptions-item>
+    <el-descriptions-item label="联系地址">甘肃省兰州市西固区杏胡台村</el-descriptions-item>
+
+    <el-descriptions-item label="用户名">datou</el-descriptions-item>
+    <el-descriptions-item label="手机号">15537926097</el-descriptions-item>
+    <el-descriptions-item label="居住地">长沙市</el-descriptions-item>
+    <el-descriptions-item label="备注">
+      <el-tag size="small">学校</el-tag>
+    </el-descriptions-item>
+    <el-descriptions-item label="联系地址">湖南省长沙市长沙县美林水郡9单元901号</el-descriptions-item>
+
+    <el-descriptions-item label="用户名">xiaotou</el-descriptions-item>
+    <el-descriptions-item label="手机号">15522518300</el-descriptions-item>
+    <el-descriptions-item label="居住地">天津市</el-descriptions-item>
+    <el-descriptions-item label="备注">
+      <el-tag size="small">工作</el-tag>
+    </el-descriptions-item>
+    <el-descriptions-item label="联系地址">天津市静海区徐庄子村</el-descriptions-item>
+
+    <el-descriptions-item label="用户名">JIMS</el-descriptions-item>
+    <el-descriptions-item label="手机号">15527009891</el-descriptions-item>
+    <el-descriptions-item label="居住地">北京市</el-descriptions-item>
+    <el-descriptions-item label="备注">
+      <el-tag size="small">学校</el-tag>
+    </el-descriptions-item>
+    <el-descriptions-item label="联系地址">北京市西城区中塔园小区 2单元204</el-descriptions-item>
+
+    <el-descriptions-item label="用户名">OPAJ</el-descriptions-item>
+    <el-descriptions-item label="手机号">14436829089</el-descriptions-item>
+    <el-descriptions-item label="居住地">西安市</el-descriptions-item>
+    <el-descriptions-item label="备注">
+      <el-tag size="small">住址</el-tag>
+    </el-descriptions-item>
+    <el-descriptions-item label="联系地址">陕西省西安市渭城区东石村</el-descriptions-item>
+
+    <el-descriptions-item label="用户名">SGUN</el-descriptions-item>
+    <el-descriptions-item label="手机号">14428910863</el-descriptions-item>
+    <el-descriptions-item label="居住地">杭州市</el-descriptions-item>
+    <el-descriptions-item label="备注">
+      <el-tag size="small">学校</el-tag>
+    </el-descriptions-item>
+    <el-descriptions-item label="联系地址">浙江省杭州市上城区金都华府</el-descriptions-item>
+
+    <el-descriptions-item label="用户名">DAWANG</el-descriptions-item>
+    <el-descriptions-item label="手机号">18937672819</el-descriptions-item>
+    <el-descriptions-item label="居住地">济南市</el-descriptions-item>
+    <el-descriptions-item label="备注">
+      <el-tag size="small">住址</el-tag>
+    </el-descriptions-item>
+    <el-descriptions-item label="联系地址">山东省济南市中井庄村荆山庄</el-descriptions-item>
+
+    <el-descriptions-item label="用户名">SIOZ</el-descriptions-item>
+    <el-descriptions-item label="手机号">15678392012</el-descriptions-item>
+    <el-descriptions-item label="居住地">北京市</el-descriptions-item>
+    <el-descriptions-item label="备注">
+      <el-tag size="small">学校</el-tag>
+    </el-descriptions-item>
+    <el-descriptions-item label="联系地址">北京市丰台区彩虹家园北区 3单元345号</el-descriptions-item>
+
+    <el-descriptions-item label="用户名">XGUN</el-descriptions-item>
+    <el-descriptions-item label="手机号">13245551654</el-descriptions-item>
+    <el-descriptions-item label="居住地">吉林市</el-descriptions-item>
+    <el-descriptions-item label="备注">
+      <el-tag size="small">住址</el-tag>
+    </el-descriptions-item>
+    <el-descriptions-item label="联系地址">吉林省吉林市丰满区永春胡同 1109号</el-descriptions-item>
+
+    <el-descriptions-item label="用户名">WAQO</el-descriptions-item>
+    <el-descriptions-item label="手机号">11567282765</el-descriptions-item>
+    <el-descriptions-item label="居住地">太原市</el-descriptions-item>
+    <el-descriptions-item label="备注">
+      <el-tag size="small">学校</el-tag>
+    </el-descriptions-item>
+    <el-descriptions-item label="联系地址">山西省太原市万柏林区移村 8道807户</el-descriptions-item>
+
+    <el-descriptions-item label="用户名">XZOP</el-descriptions-item>
+    <el-descriptions-item label="手机号">17188829785</el-descriptions-item>
+    <el-descriptions-item label="居住地">昆明市</el-descriptions-item>
+    <el-descriptions-item label="备注">
+      <el-tag size="small">工作</el-tag>
+    </el-descriptions-item>
+    <el-descriptions-item label="联系地址">云南省昆明市官渡区丁家村</el-descriptions-item>
+
+    <el-descriptions-item label="用户名">ZMMM</el-descriptions-item>
+    <el-descriptions-item label="手机号">17898889028</el-descriptions-item>
+    <el-descriptions-item label="居住地">贵阳市</el-descriptions-item>
+    <el-descriptions-item label="备注">
+      <el-tag size="small">工作</el-tag>
+    </el-descriptions-item>
+    <el-descriptions-item label="联系地址">贵州省贵阳市观山湖区上麦村</el-descriptions-item>
+
+    <el-descriptions-item label="用户名">LPQU</el-descriptions-item>
+    <el-descriptions-item label="手机号">17789090999</el-descriptions-item>
+    <el-descriptions-item label="居住地">天津市</el-descriptions-item>
+    <el-descriptions-item label="备注">
+      <el-tag size="small">学校</el-tag>
+    </el-descriptions-item>
+    <el-descriptions-item label="联系地址">天津市西青区西清道7789号</el-descriptions-item>
+
+    <el-descriptions-item label="用户名">ZHUO</el-descriptions-item>
+    <el-descriptions-item label="手机号">17728635552</el-descriptions-item>
+    <el-descriptions-item label="居住地">淮北市</el-descriptions-item>
+    <el-descriptions-item label="备注">
+      <el-tag size="small">住址</el-tag>
+    </el-descriptions-item>
+    <el-descriptions-item label="联系地址">安徽省淮北市烈山区后丁楼郭庄</el-descriptions-item>
+
+    <el-descriptions-item label="用户名">ffrr</el-descriptions-item>
+    <el-descriptions-item label="手机号">17125556562</el-descriptions-item>
+    <el-descriptions-item label="居住地">上海市</el-descriptions-item>
+    <el-descriptions-item label="备注">
+      <el-tag size="small">学校</el-tag>
+    </el-descriptions-item>
+    <el-descriptions-item label="联系地址">上海市金山区万和家园 9单元309号</el-descriptions-item>
+
+    <el-descriptions-item label="用户名">QWNH</el-descriptions-item>
+    <el-descriptions-item label="手机号">17233361627</el-descriptions-item>
+    <el-descriptions-item label="居住地">汕头市</el-descriptions-item>
+    <el-descriptions-item label="备注">
+      <el-tag size="small">工作</el-tag>
+    </el-descriptions-item>
+    <el-descriptions-item label="联系地址">广东省汕头市汇涛花区9栋901号</el-descriptions-item>
+
+    <el-descriptions-item label="用户名">ZSXA</el-descriptions-item>
+    <el-descriptions-item label="手机号">174889302778</el-descriptions-item>
+    <el-descriptions-item label="居住地">黄石市</el-descriptions-item>
+    <el-descriptions-item label="备注">
+      <el-tag size="small">住址</el-tag>
+    </el-descriptions-item>
+    <el-descriptions-item label="联系地址">湖北省黄石市下陆区卫王村</el-descriptions-item>
+
+    <el-descriptions-item label="用户名">UIOP</el-descriptions-item>
+    <el-descriptions-item label="手机号">17829039991</el-descriptions-item>
+    <el-descriptions-item label="居住地">丹东市</el-descriptions-item>
+    <el-descriptions-item label="备注">
+      <el-tag size="small">学校</el-tag>
+    </el-descriptions-item>
+    <el-descriptions-item label="联系地址">辽宁省丹东市刘家屯</el-descriptions-item>
+
+    <el-descriptions-item label="用户名">CVTDGU</el-descriptions-item>
+    <el-descriptions-item label="手机号">18819029188</el-descriptions-item>
+    <el-descriptions-item label="居住地">烟台市</el-descriptions-item>
+    <el-descriptions-item label="备注">
+      <el-tag size="small">住址</el-tag>
+    </el-descriptions-item>
+    <el-descriptions-item label="联系地址">山东省烟台市莱山区鹿鸣小区3单元306号</el-descriptions-item>
+
+    <el-descriptions-item label="用户名">NKOAS</el-descriptions-item>
+    <el-descriptions-item label="手机号">18893019910</el-descriptions-item>
+    <el-descriptions-item label="居住地">上海市</el-descriptions-item>
+    <el-descriptions-item label="备注">
+      <el-tag size="small">工作</el-tag>
+    </el-descriptions-item>
+    <el-descriptions-item label="联系地址">上海市姑苏区汇翠花园 3单元 309号</el-descriptions-item>
+
+    <el-descriptions-item label="用户名">ZCMIOP</el-descriptions-item>
+    <el-descriptions-item label="手机号">14526717718</el-descriptions-item>
+    <el-descriptions-item label="居住地">天津市</el-descriptions-item>
+    <el-descriptions-item label="备注">
+      <el-tag size="small">学校</el-tag>
+    </el-descriptions-item>
+    <el-descriptions-item label="联系地址">天津市滨海新区南海路 198号</el-descriptions-item>
+
+    <el-descriptions-item label="用户名">BMPWQ</el-descriptions-item>
+    <el-descriptions-item label="手机号">188477381789</el-descriptions-item>
+    <el-descriptions-item label="居住地">上海市</el-descriptions-item>
+    <el-descriptions-item label="备注">
+      <el-tag size="small">住址</el-tag>
+    </el-descriptions-item>
+    <el-descriptions-item label="联系地址">上海市嘉定区州桥小区5栋513号</el-descriptions-item>
+
+    <el-descriptions-item label="用户名">zdrwe</el-descriptions-item>
+    <el-descriptions-item label="手机号">15677892877</el-descriptions-item>
+    <el-descriptions-item label="居住地">重庆市</el-descriptions-item>
+    <el-descriptions-item label="备注">
+      <el-tag size="small">工作</el-tag>
+    </el-descriptions-item>
+    <el-descriptions-item label="联系地址">重庆市南岸区茶园村 990号</el-descriptions-item>
+
+    <el-descriptions-item label="用户名">zopppp</el-descriptions-item>
+    <el-descriptions-item label="手机号">15563782888</el-descriptions-item>
+    <el-descriptions-item label="居住地">重庆市</el-descriptions-item>
+    <el-descriptions-item label="备注">
+      <el-tag size="small">住址</el-tag>
+    </el-descriptions-item>
+    <el-descriptions-item label="联系地址">重庆市巴南区姚家湾 708号</el-descriptions-item>
+
+
+
   </el-descriptions>
 
 
